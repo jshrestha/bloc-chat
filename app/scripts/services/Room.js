70e342ab2e3c31be8d3f326943806e8178304f40
Jeevan Shrestha <shresthajeevan@gmail.com>
2017-09-28 11:08:45 -0700

    messages added

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,11 +1,18 @@
-((function() {
+(function() {
     function Room($firebaseArray) {
         var firebaseRef = new Firebase("https://bloc-chat-ea0c9.firebaseio.com/");
 
         var rooms = $firebaseArray(firebaseRef.child('rooms'));
 
+        var getMessages = function(roomId) {
+            var messages = firebaseRef.child('messages');
+
+            return $firebaseArray(messages.orderByChild("roomId").equalTo(roomId));
+        }
+
         return {
-            rooms
+            rooms,
+            getMessages
         };
     }
 
